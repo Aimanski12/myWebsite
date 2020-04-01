5a51bd26a682e8cfe09f093a1a3fdd65bab77204
Leo espinoza <leo.spinoza@yahoo.com>
2020-03-31 21:24:51 -0500

    :sunglasses: added code for intro

--- src/components/Intro/Two/Intro.js
+++ src/components/Intro/Two/Intro.js
@@ -14,18 +14,6 @@ function Intro () {
 
   return (
     <div className="introTwo">
-      <div className="face">
-        <img src={face} alt=""/>
-      </div>
-      <div className="headerText">
-       <h1>Hi, it's me <span className='green'>Aiman</span>.</h1>
-       <h2>I'm a <span className='green'>UX/UI</span> Designer / <span className='green'>Web/App</span> Developer</h2>
-      </div>
-      <div className="button">
-        <span>Explore Me</span>
-        <div className="btnBackground"></div>
-        {/* <img src="" alt=""/> */}
-      </div>
     </div>
   )
 }

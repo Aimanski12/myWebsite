400107e0c8471b8d3ecb6b648e84b43705dd377b
Leo espinoza <leo.spinoza@yahoo.com>
2020-04-07 12:09:33 -0500

    :sunglasses: making it better

--- src/common/Helpers/common.js
+++ src/common/Helpers/common.js
@@ -7,12 +7,36 @@ export function colorGenerator() {
     // red
     ['CB4747', '906565', '5F5757', '502D2D', 'red', '5A3535'],
     // yellow
-    ['b6ab3c', '908F65', '5E5F57', '504F2D', 'yellow', '544f35']
+    ['b6ab3c', '908F65', '5E5F57', '504F2D', 'yellow', '544f35'],
+    // orange
+    ['b6823c', '907C65', '5F5C57', '50412D', 'orange', '544535'],
+    // blue
+    ['3C97B6', '658490', '575C5F', '2D4750', 'blue', '354C54'],
   ]
 
-  return color[Math.floor(Math.random() * 3)]
+  return color[Math.floor(Math.random() * 5)]
 }
 
-// green = #355452
-// yellow = #544f35
-// red = #5A3535
+// generate the background according to the provided theme color
+export function generateBackground (c) {
+  const red = process.env.PUBLIC_URL + '/assets/images/buttonRed.png'
+  const yellow = process.env.PUBLIC_URL + '/assets/images/buttonYellow.png'
+  const green = process.env.PUBLIC_URL + '/assets/images/buttonGreen.png'
+  const orange = process.env.PUBLIC_URL + '/assets/images/buttonOrange.png'
+  const blue = process.env.PUBLIC_URL + '/assets/images/buttonBlue.png'
+  if (c === 'green') {
+    return green
+  }
+  if (c === 'yellow') {
+    return yellow
+  }
+  if (c === 'red') {
+    return red
+  }
+  if (c === 'orange') {
+    return orange
+  }
+  if (c === 'blue') {
+    return blue
+  }
+}
--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,38 +1,20 @@
-import React, {useEffect} from 'react'
+import React from 'react'
+import {generateBackground} from '../../common/Helpers/common'
 import './Button.css'
 
-const red = process.env.PUBLIC_URL + '/assets/images/buttonRed.png'
-const yellow = process.env.PUBLIC_URL + '/assets/images/buttonYellow.png'
-const green = process.env.PUBLIC_URL + '/assets/images/buttonGreen.png'
-
 function Button (props) {
-
-  useEffect(() => {
-    setColor()
-  })
-
-  const setColor = (c) => {
-    if(c === 'green'){
-      return green
-    }
-    if(c === 'yellow'){
-      return yellow
-    }
-    if(c === 'red'){
-      return red
-    }
-    
-  }
+  
+  // we need to import the function generateBackground
+  // to generate a cosistent background
   return (
     <div className="button-container">
       <div className="button-background" 
-        style={{background: `url('${setColor(props.themeColor)}') no-repeat center center`}}>
+        style={{background: `url('${generateBackground(props.themeColor)}') no-repeat center center`}}>
         <button>Explore Me</button>
       </div>
     </div>
   )
 }
-
 
 export default Button
 

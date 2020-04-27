c90482474ca0221ff794cf7678cf7f0904d7b565
Leo espinoza <leo.spinoza@yahoo.com>
2020-04-27 11:46:24 -0500

    :sunglasses: making it better

--- src/common/HelperScripts/genBkgd.js
+++ src/common/HelperScripts/genBkgd.js
@@ -1,23 +1,23 @@
 // generate the background according to the provided theme color
-export function generateBackground(c) {
-  const red = process.env.PUBLIC_URL + '/assets/images/buttonRed.png'
-  const yellow = process.env.PUBLIC_URL + '/assets/images/buttonYellow.png'
-  const green = process.env.PUBLIC_URL + '/assets/images/buttonGreen.png'
-  const orange = process.env.PUBLIC_URL + '/assets/images/buttonOrange.png'
-  const blue = process.env.PUBLIC_URL + '/assets/images/buttonBlue.png'
-  if (c === 'green') {
-    return green
-  }
-  if (c === 'yellow') {
-    return yellow
-  }
-  if (c === 'red') {
-    return red
-  }
-  if (c === 'orange') {
-    return orange
-  }
-  if (c === 'blue') {
-    return blue
-  }
-}
+// export function generateBackground(c) {
+//   const red = process.env.PUBLIC_URL + '/assets/images/buttonRed.png'
+//   const yellow = process.env.PUBLIC_URL + '/assets/images/buttonYellow.png'
+//   const green = process.env.PUBLIC_URL + '/assets/images/buttonGreen.png'
+//   const orange = process.env.PUBLIC_URL + '/assets/images/buttonOrange.png'
+//   const blue = process.env.PUBLIC_URL + '/assets/images/buttonBlue.png'
+//   if (c === 'green') {
+//     return green
+//   }
+//   if (c === 'yellow') {
+//     return yellow
+//   }
+//   if (c === 'red') {
+//     return red
+//   }
+//   if (c === 'orange') {
+//     return orange
+//   }
+//   if (c === 'blue') {
+//     return blue
+//   }
+// }

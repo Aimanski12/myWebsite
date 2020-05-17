740edb5a36cbe1f5d2837065949d12bdcdfbb90d
Leo espinoza <leo.spinoza@yahoo.com>
2020-05-17 14:12:34 -0500

    :sunglasses: added project component

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const btn = document.querySelectorAll('.card-cards .card-overlay')
+const crd = document.querySelectorAll('.crd-prj-card')
+// const crdfrt = document.querySelectorAll(' .crd-layover')
 
-const workbtn = document.querySelector('.work-btn button')
 
-for (let x = 0; x < 4; x++){
-  btn[x].addEventListener('mouseover', ()=>{
-    btn[x].classList.toggle('fadeIn')
+let a = [0,0,0,0,0,0,0,0]
+
+crd.forEach((i, x )=> {
+  i.addEventListener('click', ()=>{
+
+    if(!a[x]) {
+      i.classList.add('flip')
+      i.firstElementChild.firstElementChild.style.display = 'none'
+      a[x] = 1
+    } else {
+      i.classList.remove('flip')
+      i.firstElementChild.firstElementChild.style.display = 'flex'
+      a[x] = 0
+    }
+
+
   })
-  btn[x].addEventListener('mouseout', () => {
-    btn[x].classList.toggle('fadeIn')
-  })
-}
-
-workbtn.addEventListener('mouseenter', ()=>{
-  workbtn.classList.toggle('fadeBtn')
-})
-
-workbtn.addEventListener('mouseout', () => {
-  workbtn.classList.toggle('fadeBtn')
-})
+});
 
 
 

13294fee344545ae5f02fbe185ffc70d98a246d1
Leo espinoza <leo.spinoza@yahoo.com>
2020-07-28 16:22:47 -0500

    :sunglasses: testing

--- src/components/FooterItem/SocialIcons/SocialIcons.js
+++ src/components/FooterItem/SocialIcons/SocialIcons.js
@@ -1,31 +1,31 @@
-// import React from 'react'
-// import Social from '../../Svgs/Socials/Socials'
+import React from 'react'
+import Social from '../../Svgs/Socials/Socials'
 
-// import {findData} from '../../../utils/data/index'
-// // import './socialIcons.css'
+import {findData} from '../../../utils/data/index'
+// import './socialIcons.css'
 
-// function SocialIcons(props) {
-//   let data = findData('socials')
+function SocialIcons(props) {
+  let data = findData('socials')
   
 
-//   const socialsIcon = () => {
-//     return data.socials.map((social, i) => {
-//       return (  <a href={social.link} key={i}
-//                   rel='noopener noreferrer'
-//                   target='_blank' >
-//                   <Social 
-//                     icon={social.name}
-//                     color={props.colorMode}/>
-//                 </a>
-//       )
-//     })
-//   }
+  const socialsIcon = () => {
+    return data.socials.map((social, i) => {
+      return (  <a href={social.link} key={i}
+                  rel='noopener noreferrer'
+                  target='_blank' >
+                  <Social 
+                    icon={social.name}
+                    color={props.colorMode}/>
+                </a>
+      )
+    })
+  }
 
-//   return (
-//     <div className={`content-center ${props.container}`}>
-//       {socialsIcon()}
-//     </div>
-//   )
-// }
+  return (
+    <div className={`content-center ${props.container}`}>
+      {socialsIcon()}
+    </div>
+  )
+}
 
-// export default SocialIcons
+export default SocialIcons

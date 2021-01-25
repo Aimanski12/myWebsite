85176baa225f76c436507f9820707fd9a019ad8e
Leo espinoza <leo.spinoza@yahoo.com>
2021-01-25 17:41:16 -0600

    remove files for new website design

--- src/containers/Privacy/components/Date.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Date(props) {
-  return (
-    <h4 className="p-wrapper head4">Date: {props.date}</h4>
-  )
-}
-
-export default Date
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,6 @@
-import React, {useEffect, useContext} from 'react'
-import Head from 'next/head'
-import Intro from '../components/Intro/Intro'
-import TopLayover from '../components/TopLayover/TopLayover'
-import Navbar from '../components/Navbar/Navbar'
-import Menu from '../components/MenuContainer/MenuContainer'
-import Header from '../components/Header/Header'
-import SectionBlocks from '../components/SectionBlocks/SectionBlocks'
-import ShortProjectView from '../components/ShortProjectView/ShortProjectView'
-import Needhelp from '../components/NeedHelp/NeedHelp'
-import Footer from '../components/Footer/Footer'
-import {resize} from '../utils/common/common'
-import {scrollAnimation, debounce} from '../utils/pageanimations/scrollanimation/scrollanimation'
-import {PageDataContext} from '../utils/context/pageContext'
-import {StateContext} from '../utils/context/stateContext'
-import {variants} from '../utils/pageanimations/motion/mainvariant'
-import {motion} from 'framer-motion'
-import Fade from 'react-reveal/Fade'
-import Slide from 'react-reveal/Slide'
+import React, {useEffect} from 'react'
 
 function Home () {
-  const {state} = useContext(StateContext)
 
   useEffect(()=>{
     // resize event to when the browser is resized
@@ -29,58 +10,9 @@ function Home () {
   })
 
   return (
-    <motion.div 
-      variants={variants}
-      initial='initial'
-      animate='enter'
-      exit={state.exitMode}
-      className="container">
-      <Head>
-        <title>Aiman Adlawan | Official Website</title>
-        <link rel="icon" href="/images/brand-icon-logo.ico" />
-      </Head>
-      <div className="container">
-        <div className="fix-div"></div>
-        <div className="div one"></div>
-
-        <div className="div two">
-          <Slide>
-            <Fade bottom>
-              <h1>Hi! I’m Aiman Adlawan. A passionate front-end  developer based in Bensenville, Illinois.</h1>
-            </Fade>
-          </Slide>
-          <Slide>
-            <Fade bottom>
-              <h1>DAmple</h1>
-            </Fade>
-          </Slide>
-          <Slide>
-            <Fade bottom>
-              <h1>DAmple</h1>
-            </Fade>
-          </Slide>
-        </div>
-        <div className="div three"></div>
-        <div className="div four"></div>
-
-        {/* <Intro withAnim={true}/>
-        <TopLayover/>
-        <div className="main-container">
-          <PageDataContext pagesets={'home'}>
-            <Navbar />       
-            <Menu />
-            <main className='main'>
-              <Header/>
-              <SectionBlocks title='project' />
-              <ShortProjectView />
-              <SectionBlocks title='view more' />
-              <Needhelp />
-              <Footer />
-            </main>
-          </PageDataContext>
-        </div> */}
-      </div>
-    </motion.div>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 

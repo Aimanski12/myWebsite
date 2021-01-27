45b7203b58ebbc27bb0596c6ff6435ce2625c617
Leo espinoza <leo.spinoza@yahoo.com>
2021-01-26 22:47:29 -0600

    :sunglasses: added files for artwork :star:

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,72 +4,7 @@ import {StateProvider} from '../utils/context/stateContext'
 
 // global css
 import '../../styles/global/styleOne.css'
-import '../../styles/global/styleTwo.css'
 
-// intro css
-import '../../styles/intro/intro.css'
-
-// css for the toplayover
-import '../../styles/toplayover/toplayover.css'
-
-// css for navbar
-import '../../styles/navbar/navbar.css'
-import '../../styles/navbar/navbarlogo.css'
-import '../../styles/navbar/navbarmenu.css'
-
-// css for menu, menu runner and menu list
-import '../../styles/menu/menucontainer.css'
-import '../../styles/menu/menuwrapper.css'
-import '../../styles/menu/menurunner.css'
-import '../../styles/menu/menu.css'
-
-// css for the footer and copyright item
-import '../../styles/footer/copyright.css'
-import '../../styles/footer/socialIcons.css'
-import '../../styles/footer/footermenu.css'
-import '../../styles/footer/footer.css'
-
-// css for header
-import '../../styles/header/header.css'
-import '../../styles/header/hero.css'
-import '../../styles/header/headertextblock.css'
-import '../../styles/header/subparagraph.css'
-import '../../styles/header/credentials.css'
-
-// privacy 
-import '../../styles/privacy/privacy.css'
-
-// not found
-import '../../styles/notfound/notfound.css'
-
-// section blocks
-import '../../styles/sectionblocks/sectionblocks.css'
-
-// short project view
-import '../../styles/shortprojectview/shortprojectview.css'
-
-// needhelp 
-import '../../styles/needhelp/needhelp.css'
-
-// css for about page and sections 
-import '../../styles/about/skills.css'
-import '../../styles/about/myreads.css'
-
-// css for message form
-import '../../styles/contacts/message.css'
-
-// css for project header
-import '../../styles/projects/projectheader.css'
-import '../../styles/projects/otherprojects.css'
-
-// css for individual project
-import '../../styles/individualproject/header.css'
-import '../../styles/individualproject/projectbody.css'
-import '../../styles/individualproject/about.css'
-import '../../styles/individualproject/categories.css'
-import '../../styles/individualproject/launchlinks.css'
-import '../../styles/individualproject/screens.css'
-import '../../styles/individualproject/tools.css'
 
 function MyApp ({Component, pageProps, router}){
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,29 @@
-import React, {useEffect} from 'react'
+import React from 'react'
+import art from '../mockdata/raw/artwork.json'
 
 function Home () {
-
-  useEffect(()=>{
-    // resize event to when the browser is resized
-    // resize()
-    // scroll event
-    // window.addEventListener('scroll', debounce(scrollAnimation, 30))
-  })
+  console.log(art)
+  
+  const artwork = () => {
+    return art.map((a, i) => {
+      return(
+        <div className="wrap" 
+          style={{background: a.background}}   
+          key={i}>
+          <img src={a.image} alt=""/>
+        </div>
+      )
+    })
+  }
 
   return (
     <div>
       <h1>Hello World</h1>
+      <div className="wrap" 
+          style={{background: '#4de0db'}}>
+          <img src='/radio_live_parallax.webp' alt=""/>
+        </div>
+      {artwork()}
     </div>
   )
 }

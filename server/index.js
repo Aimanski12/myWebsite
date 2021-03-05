c7f67dbe7067bd77f7a12d33b3f1b00f06e717e7
Leo espinoza <leo.spinoza@yahoo.com>
2021-03-05 12:31:21 -0600

    :sunglasses: updated env vars :star:

--- server/index.js
+++ server/index.js
@@ -17,38 +17,35 @@ app.use(cors(corsOptions));
 
 
 app.post('/api/sendemail', async (req, res) => {
-  const { email, name} = req.body;
+  const {email, name, message} = req.body;
   try {
-
-    // await sendConfirmationEmail({email, name})
-    sendEmail('asdf')
+    sendEmail(email, name, message)
   } catch(e) {
     res.status(422).send(e.message);
   }
 })
 
 
-
-function sendEmail(message) {
+function sendEmail(email, name, message) {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     requireTLS: true,
     auth: {
-      user: 'adlawanaiman@gmail.com',
-      pass: 'MyOfficial38account'
+      user: '',
+      pass: ''
     }
   });
 
   var mailOptions = {
     from: 'adlawanaiman@gmail.com',
+    to: email,
     cc: 'adlawanaiman@gmail.com',
-    to: 'luis.fernando12T@yahoo.com',
-    subject: 'Hi Ryan.', 
+    subject: `Hi ${name},`, 
     html: `
-        <h3> Hello Henry Lukas, </h3>
-        <p>Thank you for so much for reaching out to me. It is my great pleasure to be contacted by great people like you and I hope one day we collaborate on something useful for your community and organization.</p>
+        <h3> Hello ${name}, </h3>
+        <p>Thank you for so much for reaching out to me. It is my great pleasure to be contacted by great people like you and I hope one day we collaborate on something valuable for your community and organization.</p>
 
         <p>I am currently out of office as of this time and rest assured I will get back to you very soon to see how I can be of help to amazing people like you.</p>
 
@@ -64,11 +61,11 @@ function sendEmail(message) {
         <br>
         <hr>
         <br>
-        <br>
         <p>To: Aiman Adlawan</p>
-        <p>From: Henry Lukas</p>
+        <p>From: ${email}, ${name}</p>
         <p>Date: May 1, 2021, Friday<p>
-        <p>Hi Aiman. Just wanna know if you are interested in doing this.</p>`
+        <p>${message}</p>`
+
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
